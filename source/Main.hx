98d0c994948727f9355870d30d3386e791fe34ec
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-14 10:34:55 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.graphics.FlxGraphic;
 import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
@@ -68,6 +69,8 @@ class Main extends Sprite
 		#if !debug
 		initialState = TitleState;
 		#end
+		
+		FlxGraphic.defaultPersist = true;
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 		
